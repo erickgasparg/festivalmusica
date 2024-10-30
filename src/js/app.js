16aa811d5d59d73d03954fe8a68d3cb31a7a34f7
erickgasparg <erickgasparg@gmail.com>
2024-10-30 13:25:22 -0500

    Performance imagenes Avif
    Cambiando las imágenes de jpg a avif o webp con Javascript

--- src/js/app.js
+++ src/js/app.js
@@ -26,12 +26,12 @@ function crearGaleria() {
 
     const CANTIDAD_IMAGENES= 16
     for(let i=1; i <= CANTIDAD_IMAGENES; i++){
-        const imagen = document.createElement('IMG')
-        imagen.loading = 'lazy'
-        imagen.width = '300'
-        imagen.height = '200'
-        imagen.src = `src/img/gallery/thumb/${i}.jpg`
-        imagen.alt = 'Imagen galería'
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+            <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+        `;
         
 
         //Even handler es el proceso de detectar y responder a la interacción del usuario
@@ -48,9 +48,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i){
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = 'Imagen galería'
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+    `;
 
 
     //Generar Modal
